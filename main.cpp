3adaf1ead19f0bdd7ff291c2749cf8b769add14e
avaerian <avaeriandev@gmail.com>
2026-07-23 03:17:42 -0400

    Implement basic skeleton for running application

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <stdio.h>
+#include <cstdlib>
+#include <cstdio>
+#include <stdexcept>
+#include <inttypes.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include "app.hpp"
 
 int main(int argc, char* argv[]) {
-    printf("Hello, world from C++\n"); 
-    return 0;
+    App app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        fprintf(stderr, "%s\n", e.what());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
